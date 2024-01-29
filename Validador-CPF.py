502525d66c1c5aeebb327f31b1ae5e7140e9525b
Michel <michelnropke@gmail.com>
2024-01-29 18:52:11 -0300

    Acertos no fatiamento.

--- Validador-CPF.py
+++ Validador-CPF.py
@@ -3,10 +3,19 @@ cpf_lista = [5788661706, 11501643797, 23392206709]
 cpf_soma = 0
 multi = 0
 
+# Converte o CPF para texto (str) para que possamos fatiar
 cpf = str(cpf_lista[0])
 
+# Acertando o multiplicador caso o CPF inicie com 0
+if len(cpf) < 11:
+    multi = 9
+else:
+   multi = 10
+
+# Calculo para o primeiro digito.
 for n in cpf[:-2]:
     cpf_conv = int(n)
     cpf_soma += cpf_conv * multi
+    multi += -1
 
-    print(n)
+    print(multi)
