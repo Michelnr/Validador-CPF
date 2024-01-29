d0e8166e4c457b868c9623762483c68db3fd7343
Michel <michelnropke@gmail.com>
2024-01-29 12:41:07 -0300

    Foi resolvido o problema do fatiamento do CPF

--- Validador-CPF.py
+++ Validador-CPF.py
@@ -1,11 +1,13 @@
 cpf_lista = [5788661706, 11501643797, 23392206709]
 
-# cpf = input("informe o CPF para validação: ")
+cpf = str(cpf_lista[0])
 
 cpf_soma = 0
 multi = 10
 
-for n in cpf_lista:
-#    cont = multi
-#    cpf_soma += 0
-    print(n)
+for n in cpf:
+    cpf_conv = int(n)
+    if len(cpf) < 11:
+        multi = 9
+    cpf_soma += cpf_conv
+    print(cpf_soma)
