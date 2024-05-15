7692442eefdd4172bc6fbb8d8e466f16046c6bc9
Michel <michelnropke@gmail.com>
2024-05-15 18:07:30 -0300

    Update Validador-CPF.py

--- Validador-CPF.py
+++ Validador-CPF.py
@@ -1,5 +1,4 @@
-# Este programa irá validar se um CPF é valido ou não utilizando um algoritmo específico.
-
+# Este programa irá validar se um CPF é valido ou não.
 cpf_lista = [5788661706, 11501643797, 23392206709]
 
 cpf_soma = 0
