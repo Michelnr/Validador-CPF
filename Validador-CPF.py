4085c3bb8e3c1e21d0406ee7859868fad73b466c
Michel <michelnropke@gmail.com>
2024-05-17 12:27:33 -0300

    Update Validador-CPF.py

--- Validador-CPF.py
+++ Validador-CPF.py
@@ -3,21 +3,22 @@ cpf_lista = ['05788661706', '11501643797', '23392206709']
 
 cpf_soma = 0
 multi = 10
-CPF_Dig = 0
+cpf_dig = 0
 
 # Converte o CPF para numero (int) para que possamos somar
 cpf = (cpf_lista[0])
 
 # Calculo do primeiro digito
 while multi >= 2:
-    cpf_soma += cpf[CPF_Dig] * multi
+    cpf_soma += cpf[cpf_dig] * multi
     multi -= 1
-    CPF_Dig += 1
+    cpf_dig += 1
 
-CPF_Dig = 1
+cpf_dig = 1
+cpf_soma = 0
 
 # Calculo do segundo digito.
 while multi >= 2:
     cpf_soma += cpf[0] * multi
     multi -= 1
-    CPF_Dig += 1
+    cpf_dig += 1
