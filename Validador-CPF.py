bfde649709892992cde42ceb714307846749efee
Michel <148887903+Michelnr@users.noreply.github.com>
2025-04-24 22:00:47 +0000

    criando uma DEF

--- Validador-CPF.py
+++ Validador-CPF.py
@@ -1,27 +1,15 @@
 # Este programa irá validar se um CPF é valido ou não.
 cpf_lista = ['05788661706', '11501643797', '23392206709']
 
-cpf_soma = 0
-multi = 10
-cpf_dig = 0
+def digitos(digito):
+    cont = 10
+    soma = 0
+    digito = 0
+    for digito in primeiro_digito:
+        soma += digito * cont
+    digito = soma % 11
 
-# Converte o CPF para numero (int) para que possamos somar
-cpf = (cpf_lista[0])
-cpf_fat = int(cpf[cpf_dig])
+    return digito
 
-# Calculo do primeiro digito
-while multi != 2:
-    #cpf_soma =+ cpf_fat * multi
-    print(cpf_dig)
-    multi -= 1
-    cpf_dig += 1
-    
-#cpf_dig = 1
-#cpf_soma = 0
-
-# Calculo do segundo digito.
-#while multi >= 2:
-#    cpf_soma += cpf[0] * multi
-#    multi -= 1
-#    cpf_dig += 1
-print(cpf_dig)
+for cpf in cpf_lista:
+    digitos(cpf)
